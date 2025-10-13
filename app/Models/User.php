222c22223f157b2edddaceb2730d6e9792e3cc3c
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-10-13 17:18:28 +0530

    added filament admin panel with shield plugin

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\Contracts\OAuthenticatable;
 use Laravel\Passport\HasApiTokens;
 use Spatie\OneTimePasswords\Models\Concerns\HasOneTimePasswords;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements OAuthenticatable
 {
-    use HasFactory, Notifiable, HasOneTimePasswords, HasApiTokens;
+    use HasFactory, Notifiable, HasOneTimePasswords, HasApiTokens, HasRoles;
 
     protected $fillable = [
         'name',
